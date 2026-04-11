911f8d2c67affaa777ecf21d67dca4ed54b551e1
kaos1111 <kayden.ricecrack@gmail.com>
2026-04-12 01:55:43 +0930

    Update server.js

--- server.js
+++ server.js
@@ -10,43 +10,76 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-// In-memory storage
-let messages = [];
-let users = {}; // socket.id -> username
+// rooms: { roomName: [messages] }
+const messages = {
+  general: [],
+  random: []
+};
+
+const users = {}; // socket.id -> username
 
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
-  // Send chat history
-  socket.emit("chat-history", messages);
-
-  // Set username
-  socket.on("set-username", (username) => {
+  socket.on("join", ({ username, room }) => {
     users[socket.id] = username;
-    io.emit("user-list", Object.values(users));
+    socket.join(room);
+
+    socket.room = room;
+
+    // send history
+    socket.emit("chat-history", messages[room]);
+
+    // update user list
+    updateUsers(room);
+
+    // join message
+    socket.to(room).emit("system-message", {
+      text: `${username} joined #${room}`,
+      time: new Date().toLocaleTimeString()
+    });
   });
 
-  // Send message
   socket.on("send-message", (msg) => {
+    const room = socket.room;
     const message = {
-      user: users[socket.id] || "Anonymous",
+      user: users[socket.id],
       text: msg,
       time: new Date().toLocaleTimeString()
     };
 
-    messages.push(message);
-    if (messages.length > 100) messages.shift();
+    messages[room].push(message);
+    if (messages[room].length > 200) messages[room].shift();
 
-    io.emit("receive-message", message);
+    io.to(room).emit("receive-message", message);
   });
 
-  // Disconnect
+  socket.on("typing", () => {
+    socket.to(socket.room).emit("typing", users[socket.id]);
+  });
+
   socket.on("disconnect", () => {
+    const room = socket.room;
+    const username = users[socket.id];
+
     delete users[socket.id];
-    io.emit("user-list", Object.values(users));
+
+    if (room) {
+      updateUsers(room);
+
+      socket.to(room).emit("system-message", {
+        text: `${username} left`,
+        time: new Date().toLocaleTimeString()
+      });
+    }
   });
+
+  function updateUsers(room) {
+    const roomUsers = Object.values(users);
+    io.to(room).emit("user-list", roomUsers);
+  }
 });
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on", PORT);
 });
